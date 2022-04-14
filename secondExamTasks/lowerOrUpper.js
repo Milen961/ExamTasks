f2da80f0b499695afab7edf085baf8a4a6a5994f
Milen961 <theheller178@gmail.com>
2022-04-14 11:58:20 +0300

    Adding an exam task

---
+++ secondExamTasks/lowerOrUpper.js
@@ -0,0 +1,12 @@
+function lowerOrUpper(charInput){
+    if (charInput === charInput.toUpperCase()){
+        console.log("upper-case");
+
+    }else {
+        console.log("lower-case");
+    }
+
+
+}
+lowerOrUpper('L')
+lowerOrUpper('f')
