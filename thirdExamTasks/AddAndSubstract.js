426355e863afc15054f7a6d3a90d6d07035de70b
Milen961 <theheller178@gmail.com>
2022-04-25 11:20:35 +0300

    Adding a task

---
+++ thirdExamTasks/AddAndSubstract.js
@@ -0,0 +1,8 @@
+ function addAndSubstract(num1, num2, num3){
+  let totalSum = Number(num1 + num2)
+ let substractedSum = totalSum - num3 
+ console.log(substractedSum);
+ 
+}
+ addAndSubstract(23,6,10)
+
