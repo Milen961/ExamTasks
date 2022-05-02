52d2a53bfab5ea4a81e0b32cc9ab04787cf5414d
Milen961 <theheller178@gmail.com>
2022-05-02 15:28:43 +0300

    Adding a task

---
+++ thirdExamTasks/PalindromeIntegers.js
@@ -0,0 +1,20 @@
+function palindromInteger(inputArr) {
+    let arrOfNums = inputArr;
+    function isPalindrom(number) {
+        let startNum = number;
+
+        let reversedNum = Number(number.toString().split("").reverse().join(""));
+        if (startNum === reversedNum) {
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+    for (let index = 0; index < arrOfNums.length; index++) {
+        let currentNum = arrOfNums[index];
+        console.log(isPalindrom(currentNum));
+
+    }
+}
+palindromInteger([123, 323, 421, 121]);
