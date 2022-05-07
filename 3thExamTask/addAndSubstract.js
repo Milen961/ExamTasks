dd9a5faf7440a09b278c7ae17a23ab01445efe8c
Milen961 <theheller178@gmail.com>
2022-05-07 17:26:18 +0300

    Adding a task

---
+++ 3thExamTask/addAndSubstract.js
@@ -0,0 +1,23 @@
+function addAndSubstract(arr){
+let myArr = arr;
+let sumOfOriginalArray = 0;
+let modifiedArraySum = 0;
+
+for (let index = 0; index < myArr.length; index++) {
+    sumOfOriginalArray += myArr[index];
+
+   if(myArr[index] % 2 === 0){
+       myArr[index] += index;
+
+   }else {
+       myArr[index] -= index;
+
+   }
+   modifiedArraySum += myArr[index]
+   
+}
+console.log(myArr);
+console.log(sumOfOriginalArray);
+console.log(modifiedArraySum);
+}
+addAndSubstract([5, 15, 23, 56, 35])
