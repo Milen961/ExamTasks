d13663c78961e5a22b027927469a1f5afd462d1a
Milen961 <theheller178@gmail.com>
2022-05-07 17:24:10 +0300

    Adding a task

---
+++ 4thExamTasks/SmallestofThreeNumbers.js
@@ -0,0 +1,28 @@
+// function smalestNumber(num1 , num2, num3){
+// //    let smallestNumber = function  (num1 , num2 , num3){
+// //         let smalestNumber = 0;
+// //         if(num1 <= num2  && num1 < num3){
+// //             smalestNumber = num1
+// //         }else if (num2 <= num1 && num2 <= num3){
+// //             smalestNumber = num2;
+       
+// //         }else {
+// //             smalestNumber = num3;
+       
+// //         }
+// //         return smalestNumber;
+       
+
+// //  };
+// let smalestNumber = (num1, num2 , num3) => Math.min(num1, num2 ,num3)
+//   console.log(smalestNumber(num1, num2 , num3));
+// }
+// smalestNumber(2, 5, 3)
+
+function smalestNumber(num1 , num2 ,num3){
+ let smalestNumber = (num1 , num2 , num3) => Math.min(num1 , num2 , num3 )
+ console.log(smalestNumber(num1, num2 , num3 ));
+}
+smalestNumber(2,
+  5,
+  3)
