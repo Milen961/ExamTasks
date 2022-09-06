d3d6de6bfc018437b69cb5aeee5a075b71dbcdb3
Milen961 <theheller178@gmail.com>
2022-09-06 19:35:33 +0300

    Adding a task

---
+++ ExamAdvanced/3thExam/Calorie Object.js
@@ -0,0 +1,11 @@
+function solve(arr){
+ let obj = {}
+ for (let index = 0; index < arr.length; index += 2) {
+      let product = arr[index];
+      let calories = arr[index + 1];
+      obj[product] = Number(calories)
+
+ }
+ console.log(obj)
+}
+solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])
